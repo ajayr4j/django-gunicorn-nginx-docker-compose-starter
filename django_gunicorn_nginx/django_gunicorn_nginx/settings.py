ba442989b60ce67f4aef83c1699c5a7185cae17a
Ajay Raj <cb.en.u4cse17607@cb.amrita.students.edu>
2021-06-21 18:36:56 +0530

    Added postgreSQL

--- django_gunicorn_nginx/django_gunicorn_nginx/settings.py
+++ django_gunicorn_nginx/django_gunicorn_nginx/settings.py
@@ -20,12 +20,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('SECRET_KEY')
-DEBUG = os.getenv('DEBUG')
-#SECURITY WARNING: don't run with debug turned on in production!
-#SECRET_KEY='django-insecure-)v_&4(n=7ykcdeg7xg00s2atj+c#2!jso+gurigvc048k6%5s*'
-#DEBUG=True
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
+DEBUG = os.getenv('DEBUG')
+
+# 'DJANGO_ALLOWED_HOSTS' should be a single string of hosts with a space between each.
+# For example: 'DJANGO_ALLOWED_HOSTS=localhost 127.0.0.1 [::1]'
 ALLOWED_HOSTS = ['*']
 
 
@@ -75,9 +75,13 @@ WSGI_APPLICATION = 'django_gunicorn_nginx.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": os.environ.get("SQL_ENGINE", "django.db.backends.sqlite3"),
+        "NAME": os.environ.get("SQL_DATABASE", os.path.join(BASE_DIR, "db.sqlite3")),
+        "USER": os.environ.get("SQL_USER", "user"),
+        "PASSWORD": os.environ.get("SQL_PASSWORD", "password"),
+        "HOST": os.environ.get("SQL_HOST", "localhost"),
+        "PORT": os.environ.get("SQL_PORT", "5432"),
     }
 }
 
